2510f175aa0b2ee1e5fdcbebab9f5b6c8d96e09f
Eita Aso <muscle03s@gmail.com>
2020-04-02 21:21:22 +0900

    create jiros-controller

---
+++ app/controllers/jiros_controller.rb
@@ -0,0 +1,5 @@
+class JirosController < ApplicationController
+
+  def show
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_122303) do
+ActiveRecord::Schema.define(version: 2020_04_02_113759) do
 
-  create_table "jiros", force: :cascade do |t|
+  create_table "jiros", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "store_id"
     t.string "picture"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_122303) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 2020_03_31_122303) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "profile_picture"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
