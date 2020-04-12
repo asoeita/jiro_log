f99a3a97332004d235452eb687e3d28770dfcd7c
Eita Aso <muscle03s@gmail.com>
2020-04-12 17:21:50 +0900

    add jiros-create-method

--- app/controllers/jiros_controller.rb
+++ app/controllers/jiros_controller.rb
@@ -1,5 +1,27 @@
 class JirosController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def show
   end
+
+  def new
+    @jiro = Jiro.new
+  end
+
+  def create
+    @jiro = current_user.jiros.build(jiro_params)
+    if @jiro.save
+      flash[:success] = "投稿しました"
+      redirect_to root_url
+    else
+      render "jiros/new"
+    end
+  end
+
+  private
+    #ストロングパラメーター
+    def jiro_params
+      params.require(:jiro).permit(:picture, :weather, :date, :waiting_time, :call, :pork, :soup, :comment)
+    end
+
 end
--- app/models/jiro.rb
+++ app/models/jiro.rb
@@ -1,2 +1,5 @@
 class Jiro < ApplicationRecord
+  belongs_to :user
+  mount_uploader :picture, ImageUploader
+
 end
