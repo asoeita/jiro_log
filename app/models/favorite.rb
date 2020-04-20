db40942389fddf33e89e501e5bf6da4fc36cc2f4
Eita Aso <muscle03s@gmail.com>
2020-04-20 22:40:52 +0900

    favoriteモデルに複合キーインデックスを追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :jiro
+  validates :user_id, {presence: true}
+  validates :jiro_id, {presence: true}
+  validates :id, uniqueness: {scope: [:user_id, :jiro_id]}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_214400) do
+ActiveRecord::Schema.define(version: 2020_04_20_124958) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_04_19_214400) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["jiro_id"], name: "index_favorites_on_jiro_id"
+    t.index ["user_id", "jiro_id"], name: "index_favorites_on_user_id_and_jiro_id", unique: true
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
